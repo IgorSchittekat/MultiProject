923e188e339acd9fce495c2c487966e31f2bd63c
IgorSchittekat <IgorSchittekat@users.noreply.github.com>
2021-10-14 12:08:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ pip3 install pytest
 pip3 install coverage
 
 #export PYTHONPATH="${PYTHONPATH}:Users/igor/Documents/Research/TestAmplification/library/flair"
-pip3 install -r requirements.txt
+pip3 install -r flair/requirements.txt
 # PYTHONPATH="/Users/igor/Documents/Research/TestAmplification/library" coverage run -m pytest library
 # coverage run -m pytest .
 
